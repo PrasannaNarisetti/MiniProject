54443b265d73ef4e85f7cbc685a8162f643413ad
PrasannaNarisetti <105403118+PrasannaNarisetti@users.noreply.github.com>
2023-07-18 16:24:20 +0530

    algo.py

--- algo.py
+++ algo.py
@@ -3,7 +3,7 @@ import pickle
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-data_for_modal = pd.read_csv("/Users/PycharmProjects/minipro/urls.csv")
+data_for_modal = pd.read_csv("minipro/urls.csv")
 x = data_for_modal.drop(['url', 'type'], axis=1).values
 y = data_for_modal['type'].values
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
